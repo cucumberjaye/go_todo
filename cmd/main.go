789e798e39d555c2b2fdc2e23c23f91cea0a8d6d
cucumberjaye <iskander.muzipov@gmail.com>
2022-07-30 21:41:38 +0300

    addbd

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,41 @@ package main
 import (
 	todo "github.com/cucumberjaye/go_todo"
 	"github.com/cucumberjaye/go_todo/pkg/handler"
+	"github.com/cucumberjaye/go_todo/pkg/repository"
+	"github.com/cucumberjaye/go_todo/pkg/service"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5436",
+		Username: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
